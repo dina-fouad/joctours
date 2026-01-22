3ce612dd6c2984cc42b825a704e3a820aade9691
dina fouad <dina_albarghouthi@yahoo.com>
2026-01-22 14:47:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Tours from "./pages/Tours";
+import Guides from "./pages/Guides";
+import Programs from "./pages/Programmes";
+import Contact from "./pages/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return (
+   
+      <Routes>
+        {/* -------------------- English -------------------- */}
+        <Route path="/en" element={<Home lang="en" />} />
+        <Route path="/en/tours" element={<Tours lang="en" />} />
+        <Route path="/en/guides" element={<Guides lang="en" />} />
+        <Route path="/en/programs" element={<Programs lang="en" />} />
+        <Route path="/en/contact" element={<Contact lang="en" />} />
+
+        {/* -------------------- Russian -------------------- */}
+        <Route path="/ru" element={<Home lang="ru" />} />
+        <Route path="/ru/tours" element={<Tours lang="ru" />} />
+        <Route path="/ru/guides" element={<Guides lang="ru" />} />
+        <Route path="/ru/programs" element={<Programs lang="ru" />} />
+        <Route path="/ru/contact" element={<Contact lang="ru" />} />
+
+       
+        <Route path="*" element={<Home lang="en" />} />
+      </Routes>
+  
   );
 }
 
